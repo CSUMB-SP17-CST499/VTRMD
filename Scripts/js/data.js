696118f9076c3bd7239997a976447abe8d933a5f
Richard Ciampa <rciampa@csumb.edu>
2017-04-09 00:34:21 -0700

    added the first async data call

--- Scripts/js/data.js
+++ Scripts/js/data.js
@@ -1,1 +1,22 @@
+/*
+ *  @author Richard Ciampa
+ *  @email rciampa@csumb.edu
+ *  
+ */
 
+var dataUri = "./cals-data-access.php?";
+
+function getAllTestLocations() {
+	dataUriCall = dataUri + "fnc=getAllTestLocations" + "&rand=" + Math.random();
+	loadAsyncData(encodeURI(dataUriCall), "GET", function() {
+		cbGetAllTestLocations();
+	}, true, null);
+}
+
+/*
+ *  Call back function for the getAllTestLocations() function call. Used to
+ *  retrieve all test locations in the CalSpeed database system
+ */
+function cbGetAllTestLocations() {
+	document.getElementById("data-content").innerHTML = xmlhttpRequest.responseText;
+}
